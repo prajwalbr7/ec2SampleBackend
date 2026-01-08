b469364e47160c3b15d7f44345151934c4277a6f
Prajwal yadav <prajwalbr7@gmail.com>
2026-01-09 00:03:05 +0530

    commited

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+let submittedData = [];
+
 app.post('/submit', (req, res) => {
+  submittedData.push(req.body);
   res.json({ message: `Hello ${req.body.name}, form submitted!` });
+});
+
+app.get('/data', (req, res) => {
+  res.json(submittedData);
+  console.log('Data retrieved:', submittedData);
 });
 
 app.listen(5000, () => console.log('Backend running on port 5000'));
